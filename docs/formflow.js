bf216c69f8f0630ecb3d0243a738ae44ee21ebfe
Nick Doiron <ndoiron@mapmeld.com>
2020-04-02 16:34:58 -0400

    separate out most of logic into a question definition

--- docs/formflow.js
+++ docs/formflow.js
@@ -18,112 +18,112 @@ function parseNumber(num) {
   );
 }
 
-var nonprofit_q = 11,
+var nonprofit_q = "q11",
   requirements = {
     eag: { // Emergency Assistance Grant
-      required_yes: [1, 2, 12, 16, 17, 20, 21, 22, 23, 42],
-      required_no: [3, 14],
+      required_yes: ["q1", "q2", "q12", "q16", "q17", "q20", "q21", "q22", "q23", "q42"],
+      required_no: ["q3", "q14"],
       eval: {
-        8: function (fte) {
+        "q8": function (fte) {
           fte = parseNumber(fte);
           return fte >= 1 && fte <= 10;
         },
-        11: function (val) {
+        "q11": function (val) {
           return (val === true) || (answers[nonprofit_q] === false);
         },
-        18: function (val) {
+        "q18": function (val) {
           return (val === true) || (val === -1); // yes or not sure
         }
       }
     },
     eawcl: { // Emergency Assistance 0% Working Capital
-      required_yes: [1, 2, 7, 16, 17, 20, 21, 22, 23, 42],
-      required_no: [3, 14],
+      required_yes: ["q1", "q2", "q7", "q16", "q17", "q20", "q21", "q22", "q23", "q42"],
+      required_no: ["q3", "q14"],
       eval: {
-        9: function (revenue) {
+        "q9": function (revenue) {
           revenue = parseNumber(revenue);
           return revenue < 5000000;
         },
-        11: function (val) {
+        "q11": function (val) {
           return (val === true) || (answers[nonprofit_q] === false);
         },
-        18: function (val) {
+        "q18": function (val) {
           return (val === true) || (val === -1); // yes or not sure
         },
-        19: function (val) {
+        "q19": function (val) {
           return (val === true) || (answers[nonprofit_q] === true);
         }
       }
     },
     guarantee: { // Emergency Assistance Guarantee
-      required_yes: [1, 2, 7, 16, 17, 20, 21, 22, 23, 42],
-      required_no: [3, 14],
+      required_yes: ["q1", "q2", "q7", "q16", "q17", "q20", "q21", "q22", "q23", "q42"],
+      required_no: ["q3", "q14"],
       eval: {
-        9: function (revenue) {
+        "q9": function (revenue) {
           revenue = parseNumber(revenue);
           return revenue < 5000000;
         },
-        11: function (val) {
+        "q11": function (val) {
           return (val === true) || (answers[nonprofit_q] === false);
         },
-        18: function (val) {
+        "q18": function (val) {
           return (val === true) || (val === -1); // yes or not sure
         },
-        19: function (val) {
+        "q19": function (val) {
           return (val === true) || (answers[nonprofit_q] === true);
         }
       }
     },
     egp: { // Entrepreneur Guarantee
-      required_yes: [1, 4, 5, 6, 13, 17, 23, 42],
-      required_no: [14],
+      required_yes: ["q1", "q4", "q5", "q6", "q13", "q17", "q23", "q42"],
+      required_no: ["q14"],
       eval: {
-        8: function (fte) {
+        "q8": function (fte) {
           fte = parseNumber(fte);
           return fte < 25;
         },
-        10: function (revenue) {
+        "q10": function (revenue) {
           revenue = parseNumber(revenue);
           return revenue < 5000000;
         }
       }
     },
     eidl: { // SBA EIDL
-      required_yes: [6, 7, 42],
-      required_no: [3, 15]
+      required_yes: ["q6", "q7", "q42"],
+      required_no: ["q3", "q15"]
     },
     frelief: { // NJ EDA relief
-      required_yes: [0, 42]
+      required_yes: ["q0", "q42"]
     },
     cdfi: {
-      required_yes: [42],
+      required_yes: ["q42"],
       eval: {
-        8: function (fte) {
+        "q8": function (fte) {
           fte = parseNumber(fte);
           return fte < 20;
         },
-        9: function (revenue) {
+        "q9": function (revenue) {
           revenue = parseNumber(revenue);
           return revenue < 1000000;
         },
-        10: function (revenue) {
+        "q10": function (revenue) {
           revenue = parseNumber(revenue);
           return revenue < 1000000;
         }
       }
     },
     bank: {
-      required_yes: [42],
+      required_yes: ["q42"],
       eval: {
-        8: function (fte) {
+        "q8": function (fte) {
           fte = parseNumber(fte);
           return fte < 20;
         },
-        9: function (revenue) {
+        "q9": function (revenue) {
           revenue = parseNumber(revenue);
           return revenue >= 100000;
         },
-        10: function (revenue) {
+        "q10": function (revenue) {
           revenue = parseNumber(revenue);
           return revenue >= 100000;
         }
@@ -132,7 +132,7 @@ var nonprofit_q = 11,
   };
 var programs = Object.keys(requirements);
 
-var answers = [];
+var answers = {};
 
 function moveToReport(e) {
   e.preventDefault();
@@ -170,7 +170,7 @@ function nextQuestion(currentQuestion) {
   while (
     ($(nextDiv).parent().css("display") === "none")
     ||
-    ($(nextDiv).find(".answered").css("color") === "rgb(170, 170, 170)")
+    ($(nextDiv).find(".answered").hasClass("already-answered"))
   ) {
     currentQuestion++;
     nextDiv = $(".question")[currentQuestion + 1];
@@ -182,7 +182,7 @@ function nextQuestion(currentQuestion) {
 
   $(nextDiv).find(".btn, div, p, li").css({ opacity: 1 });
   $(nextDiv).find('.btn, input').prop('disabled', false);
-  $(nextDiv).find('.answered').css({ display: "block" });
+  $(nextDiv).find('.answered').addClass("please-answer");
 
   programs.forEach(function (pcode) {
     var muteProgram = false,
@@ -226,64 +226,101 @@ function hardPass() {
   return false;
 }
 
-function viewGuide() {
-
-}
-
 $(document).ready(function () {
   $(".report").hide();
+
+  var outer_shell = $("#form-qs"),
+      firstQuestion = true;
+
+  Object.keys(questions).forEach(function (qcode) {
+    var q = questions[qcode],
+        shell = outer_shell;
+
+    if (q === true) {
+      shell.append($("<h3>").text(qcode));
+      return;
+    }
+
+    if (q.classes) {
+      var inner_shell = $("<div>").attr("class", q.classes);
+      shell.append(inner_shell);
+      shell = inner_shell;
+    }
+
+    var qdiv = $("<div>").attr("id", qcode).attr("class", "question").html(q.html);
+    shell.append(qdiv);
+
+    var arrow = $("<span>").attr("class", "answered").text("‣")
+    if (firstQuestion) {
+      arrow.addClass("please-answer");
+      firstQuestion = false;
+    }
+    qdiv.prepend(arrow);
+
+    if (q.input) {
+      var formplace = $("<div>").attr("class", "form-group");
+      qdiv.append(formplace);
+
+      if (q.input.label) {
+        formplace.append($("<label>").attr("for", q.input.name).text(q.input.label));
+      }
+      if (q.input.examples) {
+        formplace.append($("<small>").attr("class", "form-text text-muted").text("Examples: " + q.input.examples));
+      }
+
+      formplace.append($("<input>").attr("type", "text").attr("class", "input form-control form-control-lg").attr("name", q.input.name));
+      formplace.append($("<button>").attr("class", "btn btn-primary").attr("data-label", "Enter").text("Enter"));
+
+      if (q.skippable) {
+        formplace.append($("<button>").attr("class", "btn btn-info not-sure").attr("data-label", q.skippable.length ? q.skippable : "Skip").text(q.skippable.length ? q.skippable : "Skip"));
+      }
+    } else {
+      qdiv.append($("<button>").attr("class", "btn btn-primary").attr("data-label", q.yes_text || "Yes").text(q.yes_text || "Yes"));
+      if (q.skippable) {
+        qdiv.append($("<button>").attr("class", "btn btn-info not-sure").attr("data-label", q.skippable.length ? q.skippable : "Not Sure").text(q.skippable.length ? q.skippable : "Not Sure"));
+      }
+      qdiv.append($("<button>").attr("class", "btn btn-dark").attr("data-label", q.no_text || "No").text(q.no_text || "No"));
+    }
+  });
 
   $(".question").each(function (index) {
     var q = this;
     if (index) {
+      // hidden question styles
       $(q).find('.btn, div, p, li').css({ opacity: 0.4 });
       $(q).find('.btn, input').prop('disabled', true);
     } else {
+      // very first question (must be visible for strters)
       $(q).find('.answered').css({ display: "block" });
     }
-    $(q).find('.answered').click(function (e) {
-      $(q).find('.btn, div, p, li').css({ opacity: 1 });
-    });
-
-    var sheet_original_index = q.id.match(/\d+/)[0] * 1;
+    // $(q).find('.answered').click(function (e) {
+    //   $(q).find('.btn, div, p, li').css({ opacity: 1 });
+    // });
 
     // YES / ENTER button
     $(q).find('.btn-primary').click(function (e) {
       e.preventDefault();
-      answers[sheet_original_index] = true;
+      answers[q.id] = q.input ? $(q).find('input[name="' + q.input.name + '"]').val() : true;
 
-      if (sheet_original_index === 2) { // physically in NJ
-        $('.physical_nj').show();
-        $('.not_in_nj').hide();
-
-        if (answers[3] === false) {
-          $('.detail_qs').show();
-        } else if (answers[3] === true) {
-          $('.detail_qs').hide();
-        }
-      } else if (sheet_original_index === 3) { // home business
-        $('.detail_qs').hide();
-      } else if (sheet_original_index === 8) { // how many FTE
-        answers[sheet_original_index] = $("input[name='fte']").val();
-      } else if (sheet_original_index === 9) { // 2019 revenue
-        answers[sheet_original_index] = $("input[name='2019_revenue']").val();
-      } else if (sheet_original_index === 10) { // YTD 12-month revenue
-        answers[sheet_original_index] = $("input[name='12mo_revenue']").val();
-      } else if (sheet_original_index === 11) { // nonprofit detail button
-        $('.for-profit').hide();
-      } else if (sheet_original_index === 12) { // NAICS
-        answers[sheet_original_index] = $("input[name='12mo_revenue']").val();
-      } else if (sheet_original_index === 13) { // entrepreneur
-        $('.entrepreneur').show();
-      } else if (sheet_original_index === 14) { // NJ illegal business
+      if (questions[q.id].hard_pass === true) {
         hardPass();
-      } else if (sheet_original_index === 110) { // for-profit or non-profit
-        $('.non-profit').hide();
-        $('.for-profit').show();
-        answers[11] = undefined;
+      }
+      if (questions[q.id].yes_hides) {
+        questions[q.id].yes_hides.forEach(function (cl) {
+          $("." + cl).hide();
+        });
+      }
+      if (questions[q.id].no_hides) {
+        questions[q.id].no_hides.forEach(function (cl) {
+          $("." + cl).show();
+        });
       }
 
-      $(q).find(".answered").css({ color: "#aaa" });
+      if (q.id === "q110") { // for-profit or non-profit
+        answers["q11"] = undefined;
+      }
+
+      $(q).find(".answered").addClass("already-answered");
       $(q).find(".btn").css({ borderColor: "transparent" });
       $(e.target).css({ border: "3px solid orange" });
 
@@ -294,18 +331,20 @@ $(document).ready(function () {
     // NOT SURE / SKIP button (pre-emptive YES)
     $(q).find('.btn.not-sure').click(function (e) {
       e.preventDefault();
-      answers[sheet_original_index] = -1;
+      answers[q.id] = q.input ? undefined : -1;
 
-      // numeric answers going back to not sure / skip
-      try {
-        // old browser Array.includes
-        if ([8, 9, 10, 12].includes(sheet_original_index)) {
-          answers[sheet_original_index] = undefined;
-        }
-      } catch (e) {
+      if (questions[q.id].yes_hides) {
+        questions[q.id].yes_hides.forEach(function (cl) {
+          $("." + cl).show();
+        });
+      }
+      if (questions[q.id].no_hides) {
+        questions[q.id].no_hides.forEach(function (cl) {
+          $("." + cl).show();
+        });
       }
 
-      $(q).find(".answered").css({ color: "#888" });
+      $(q).find(".answered").addClass("already-answered");
       $(q).find(".btn").css({ borderColor: "transparent" });
       $(e.target).css({ border: "3px solid orange" });
 
@@ -316,30 +355,23 @@ $(document).ready(function () {
     // NO button
     $(q).find('.btn-dark').click(function (e) {
       e.preventDefault();
-      answers[sheet_original_index] = false;
+      answers[q.id] = false;
 
-      if (sheet_original_index === 1) { // not registers in NJ
+      if (questions[q.id].hard_pass === false) {
         hardPass();
-      } else if (sheet_original_index === 2) { // not physically in NJ
-        $('.physical_nj').hide();
-        $('.not_in_nj').show();
-      } else if (sheet_original_index === 3) { // home business
-        if (answers[2] === false) {
-          $('.detail_qs').hide();
-        } else if (answers[2] === true) {
-          $('.detail_qs').show();
-        }
-      } else if (sheet_original_index === 11) { // non-profit detail
-        $('.for-profit').show();
-      } else if (sheet_original_index === 13) { // entrepreneur
-        $('.entrepreneur').hide();
-      } else if (sheet_original_index === 42) { // agree to disclaimer
-        hardPass();
-      } else if (sheet_original_index === 110) { // for-profit or non-profit
-        $('.non-profit').show();
+      }
+      if (questions[q.id].yes_hides) {
+        questions[q.id].yes_hides.forEach(function (cl) {
+          $("." + cl).show();
+        });
+      }
+      if (questions[q.id].no_hides) {
+        questions[q.id].no_hides.forEach(function (cl) {
+          $("." + cl).hide();
+        });
       }
 
-      $(q).find(".answered").css({ color: "#888" });
+      $(q).find(".answered").addClass("already-answered");
       $(q).find(".btn").css({ borderColor: "transparent" });
       $(e.target).css({ border: "3px solid orange" });
 
@@ -348,15 +380,3 @@ $(document).ready(function () {
     });
   });
 });
-
-if (window.location.href.includes("scroll")) {
-  function scrolledStuff() {
-    if ((window.pageYOffset || window.scrollY) > $(".fixed_marker").offset().top) {
-      $('.my_options, .hidden_options').addClass('scrollme');
-    } else {
-      $('.my_options, .hidden_options').removeClass('scrollme');
-    }
-  }
-  $(window).scroll(scrolledStuff);
-  scrolledStuff(); // on page load
-}
---
+++ docs/questions.js
@@ -0,0 +1,309 @@
+var questions = {
+  "q42": {
+    html: "<strong>NJEDA Disclaimer for Organization Support Eligibility Wizard</strong> \
+    <p> \
+      As a courtesy, the NJEDA is offering this screening tool (the “Eligibility Wizard”) \
+      that lists emergency assistance programs that you may decide to consider based \
+      upon information you provide about your organization. \
+    </p> \
+    <p> \
+      To apply for a program, you need to apply through the appropriate administering \
+      agency. The Eligibility Wizard will provide suggestions on how to apply for any \
+      benefit on this website but cannot guarantee eligibility and does not reserve any funding \
+      or a place in any application queue. The administering agency \
+      will determine all eligibility requirements upon your submission of an \
+      application to that agency for the respective program. \
+    </p> \
+    <p> \
+      The information you share for purposes of this Eligibility Wizard will be \
+      anonymous and may be shared with NJ State agencies to improve access to \
+      benefits and programs. \
+    </p>",
+    hard_pass: false, // if saying no to agreement
+    yes_text: "Agree",
+    no_text: "Cancel"
+  },
+
+  "Basic Information": true,
+  "q1": {
+    hard_pass: false, // if saying no to NJ
+    html: "<p> \
+      Is your business/non-profit registered to do business in New Jersey? \
+    </p>"
+  },
+  "q110": {
+    html: "<p> \
+      Is your organization for-profit or not-for-profit? \
+    </p>",
+    yes_text: "For-profit",
+    no_text: "Not-for-profit",
+    yes_hides: ["non-profit"]
+  },
+  "q11": {
+    classes: "non-profit",
+    html: "<p> \
+      Does your not-for-profit have one of the following designations: 501(c)(3), 501(c)(4), 501(c)(7)? \
+    </p>",
+    yes_hides: ["for-profit"]
+  },
+  "q0": {
+    html: "<p> \
+      Does your organization have an existing financial relationship \
+      with NJEDA, such as a loan or incentive agreement? \
+    </p>"
+  },
+  "q7": {
+    html: "<p> \
+      Has your business been in operation for one year or more? \
+    </p>"
+  },
+
+  "Physical Location": true,
+  "q2": {
+    html: "<p> \
+      Does your organization have a physical commercial location in the State of New Jersey (e.g., a \
+      non-homebased office, \
+      a physical-retail store, a production facility, a \
+      warehouse, etc.)? \
+    </p>",
+    yes_hides: ["not_in_nj"],
+    no_hides: ["physical_nj"]
+  },
+  "q3": {
+    html: "<p> \
+      Is your business home-based? \
+    </p>",
+    yes_hides: ["detail_qs"]
+  },
+
+  "Industry": true,
+  "q12": {
+    html: "<p> \
+      Is your organization classified in one of the following industries? \
+    </p> \
+    <ul> \
+      <li>Retail</li> \
+      <li>Accommodation & food services</li> \
+      <li>Arts, entertainment & recreation </li> \
+      <li>Other services, such as repair and maintenance of vehicles, machinery, equipment, or products </li> \
+      <li>Other services, such as personal services (barbers, cleaners, nail salons, etc.) or laundry</li> \
+    </ul> \
+    <p>If you are unsure about if you fall into one of the “Other services” categories, please confirm you are \
+      in an industry with an industry code that starts with 811 or 812 on this list: \
+        <a href=\"https://www.naics.com/six-digit-naics/?code=81\" target=\"_blank\"> \
+        https://www.naics.com/six-digit-naics/?code=81</a> \
+    </p>",
+    no_text: "None of the above"
+  },
+  "q13": {
+    classes: "for-profit",
+    html: "<p> \
+      Is your organization classified in one of the following industries? \
+      <ul> \
+        <li>Technology</li> \
+        <li>Life Sciences</li> \
+        <li>Clean Energy</li> \
+        <li>Offshore wind</li> \
+        <li>Advanced transportation and logistics (does not include traditional logistics such as trucking, \
+          wholesale, or warehousing)</li> \
+        <li>Advanced manufacturing</li> \
+        <li>Finance and professional services</li> \
+        <li>Non-retail food and beverage (does not include restaurants, grocery stores, catering companies \
+          or \
+          other prepared or unprepared retail food outlets)</li> \
+        <li>Film and digital media</li> \
+      </ul> \
+    </p>",
+    no_text: "None of the above",
+    no_hides: ["entrepreneur"]
+  },
+
+  "Business Size": true,
+
+  "q8": {
+    html: "<p> \
+      How many W-2 full time equivalent (FTE) employees does \
+      your business have? \
+    </p> \
+    <ul> \
+      <li>Count 1 FTE for each full-time employee</li> \
+      <li>Only count yourself as 1 FTE if you treat yourself as a W-2 employee of the company</li> \
+      <li>For each part-time W-2 employee, estimate the number of hours \
+        worked per week, divide that number by 35, and that is the \
+        fractional FTE estimate for that employee. \
+      <li>Do not include 1099 employees in this calculation</li> \
+    </ul>",
+    input: {
+      name: "fte",
+      label: "Total number of FTEs",
+      examples: "1, 10.5, 15"
+    },
+    skippable: true
+  },
+  "q4": {
+    classes: "physical_nj entrepreneur",
+    html: "<p> \
+      Does your business have <strong>a minimum of 50% of employees in \
+        New Jersey</strong>, as reported on your NJ WR-30 at year end 2019? \
+    </p>"
+  },
+  "q5": {
+    classes: "physical_nj entrepreneur",
+    html: "<p> \
+      Is your organization's \
+      <strong>primary location / corporate headquarters in New Jersey</strong>, \
+      including at least one C-suite member with a \
+      principal office in New Jersey? \
+    </p>"
+  },
+
+  "Revenue": true,
+  "q9": {
+    html: "<p> \
+      Please estimate your business' annual revenue for the year of 2019? \
+    </p>",
+    input: {
+      name: "2019_revenue",
+      // label: "Total number of FTEs",
+      examples: "$50000; 3,000,000"
+    },
+    skippable: true
+  },
+  "q10": {
+    classes: "entrepreneur",
+    html: "<p> \
+      Please estimate your business' 12-month trailing revenue as of March 9, 2020? \
+    </p>",
+    input: {
+      name: "12mo_revenue",
+      examples: "$50000; 3,000,000"
+    },
+    skippable: true
+  },
+
+  "Loans and Guarantees": true,
+  "q16": {
+    classes: "detail_qs physical_nj",
+    html: "<p> \
+      Do you have financial statements that are \
+      CPA prepared, management prepared, or filed copies of business tax returns? \
+    </p>"
+  },
+  "q18": {
+    html: "<p> \
+      Do you, another owner, or your business have a minimum FICO score of at least 600? \
+    </p>",
+    skippable: "Not Sure"
+  },
+  "q19": {
+    classes: "for-profit",
+    html: "<p> \
+      If you are a for-profit business, would all owners with more than 10% ownership in the company be \
+      willing to provide \
+      personal guarantees for any emergency loans? \
+    </p>"
+  },
+
+  "Other": true,
+  "q17": {
+    html: "<p> \
+      Are you able to certify that you are in good standing with the \
+      New Jersey Department of Taxation? \
+      <br /> \
+      <em>Note: you may be required to provide a Tax Clearance \
+        Certificate for certain emergency assistance programs.</em> \
+    </p>",
+    skippable: "Not Sure"
+  },
+  "q23": {
+    html: "<p> \
+      Are you in good standing with the New Jersey Department of Labor? \
+      <br /> \
+      <em>Note, you will be required to be in good standing with the New Jersey \
+        Department of Labor to be eligible for State assistance. This means not \
+        having unpaid unemployment holdings, unpaid fees, or outstanding employee \
+        wage and hour violations.</em> \
+    </p>",
+    skippable: "Not Sure"
+  },
+
+  "q20": {
+    classes: "detail_qs physical_nj",
+    html: "<p> \
+      Are you, your chief executive officer, or equivalent able to certify that: \
+      <br /> \
+      The organization will make its \
+      <strong>best-effort not to furlough or lay off any individuals</strong> \
+      from the time of application through the end of the COVID-19 outbreak period; businesses that have \
+      already furloughed or laid off workers \
+      must <strong>make a best-effort to re-hire those workers as soon as possible</strong>. \
+    </p>"
+  },
+  "q22": {
+    classes: "detail_qs physical_nj",
+    html: "<p> \
+      Are you, your chief executive officer, or equivalent able to certify that: \
+      <br /> \
+      Your business has \
+      <strong>a material financial need that cannot be overcome without the use of emergency relief \
+        funds</strong> \
+      at this time (e.g., does not have significant cash reserves that can support your operations during \
+      this \
+      period of economic disruption) \
+    </p>"
+  },
+  "q21": {
+    classes: "detail_qs physical_nj",
+    html: "<p> \
+      Are you, your chief executive officer, or equivalent able to certify that: \
+      <br /> \
+      <strong>Your business been negatively impacted by the Covid-19 outbreak?</strong> \
+      Note: negative impact is defined as a business that has been temporarily shut down, has been required \
+      to \
+      reduce hours, has had at least a 20% drop in revenue, has been materially impacted by employees who \
+      cannot work due to the outbreak, or has a supply chain that has materially been disrupted and \
+      therefore \
+      slowed firm-level production. \
+    </p>"
+  },
+
+  "q14": {
+    hard_pass: true, // if saying yes to illegal business
+    html: "<p> \
+      Is your business considered one of the following: \
+      <span class=\"badge badge-success\">NJ State Programs</span> \
+    </p> \
+    <ul> \
+      <li>Related to gambling or gaming activities</li> \
+      <li>Related to the purveyance of “adult” (i.e., pornographic, lewd, prurient, obscene) activities, \
+        services, products or materials (including nude or semi-nude performances or the sale of sexual aids \
+        or devices);</li> \
+      <li>An auction or bankruptcy or fire or “lost-our-lease” or “going-out-of-business” or similar sale; \
+      </li> \
+      <li>A traveling merchant</li> \
+      <li>A Christmas tree sales or other outdoor storage;</li> \
+      <li>Any other activity constituting a nuisance</li> \
+      <li>Illegal under the laws of the State of New Jersey</li> \
+    </ul>"
+  },
+  "q15": {
+    // hard_pass: true, // if saying yes to illegal business
+    html: "<p> \
+      Is your business considered one of the following: \
+      <span class=\"badge badge-success\">Federal SBA Programs</span> \
+    </p> \
+    <ul> \
+      <li>Related to gambling or gaming activities</li> \
+      <li>Agricultural Enterprises (i.e. the primary activity of the business, including its affiliates, is \
+        as \
+        defined in Section 18(b)(1) of the Small Business Act)</li> \
+      <li>Religious organizations</li> \
+      <li>Charitable organizations</li> \
+      <li>Businesses considered hobbies</li> \
+      <li>Government-owned concerns</li> \
+      <li>Real estate developers - establishments primarily engaged in subdividing real property into lots \
+        and \
+        developing it for resale on their own account</li> \
+    </ul>"
+  }
+};

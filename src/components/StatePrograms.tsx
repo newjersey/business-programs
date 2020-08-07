25c4f15867fa18e784b127e032fc08c988ad28e2
Kenny <kennethpolyak@gmail.com>
2020-08-07 09:27:34 -0700

    sticky sidebar

--- src/components/StatePrograms.tsx
+++ src/components/StatePrograms.tsx
@@ -9,107 +9,12 @@ import Footer from "./Footer";
 import "./form-style.scss";
 import "./new_results.scss";
 
-const allStatePrograms = [
-  {
-    id: 'ca_small_biz',
-    name: 'California Small Business Finance Center',
-    what: 'Loan guarantees for small businesses',
-    who: 'California small businesses. Loan proceeds can be used for business continuance or to cure "economic injury" as a result of the COVID-19 pandemic.',
-    url: 'https://ibank.ca.gov/small-business-finance-center/',
-    status: 'Available Now'
-  },
-  {
-    id: 'pitt_elf',
-    name: 'Pittsburgh Small Business Emergency Loan Fund',
-    what: 'Loans up to $15,000 with 0% interest offered by the Urban Redevelopment Authority.',
-    who: 'Small businesses located in Pittsburgh with fewer than 30 full-time employees.',
-    url: 'https://www.ura.org/pages/covid-19-small-business-fund',
-    status: 'Available Now'
-  },
-  {
-    id: 'pitt_ura',
-    name: 'Urban Redevelopment Authority of Pittsburgh Recovery Loan',
-    what: 'Loans up to $75,000 with 0% interest for the first year and 2% for the balance of loan term.',
-    who: 'Small businesses located in Pittsburgh with fewer than 30 full-time employees.',
-    url: 'https://www.ura.org/pages/covid-19-small-business-fund',
-    status: 'Available Now'
-  },
-  {
-    id: 'pitt_bridgeway',
-    name: 'Bridgeway Capital Response Fund',
-    what: 'Connecting small businesses in western Pennsylvania to capital when it is needed most.',
-    who: 'Small businesses in western Pennsylvania.',
-    url: 'https://www.bridgewaycapital.org/loans-and-modifications/covid-19-response-fund/',
-    status: 'Available Now'
-  },
-  {
-    id: 'pitt_kiva',
-    name: 'Kiva Pittsburgh Loans',
-    what: 'Kiva helps financially excluded and socially impactful entrepreneurs access capital via crowdfunding.',
-    who: 'Small businesses located in the surrounding areas around Pittsburgh.',
-    url: 'https://www.riversidecenterforinnovation.com/kiva/home',
-    status: 'Available Now'
-  },
-  {
-    id: 'pitt_honeycomb',
-    name: 'Honeycomb Small Business Relief Loan',
-    what: 'Honeycomb Crowdfunded Small Business Relief Loan.',
-    who: 'Small businesses located in the surrounding areas around Pittsburgh.',
-    url: 'https://www.honeycombcredit.com/relief',
-    status: 'Available Now'
-  },
-  {
-    id: 'hawaii_community',
-    name: 'Hawaii Community-Based Economic Development Loan',
-    what: 'CBED offers micro-loans usually up to $50,000 to eligible small businesses that support economic development in their communities.',
-    who: '',
-    url: 'https://invest.hawaii.gov/business/cbed/',
-    status: 'Available Now'
-  },
-  {
-    id: 'hawaii_small_business',
-    name: 'Hawaii Small Business Relief & Recovery Fund',
-    what: 'Small Business Relief & Recovery Fund offers a one-time reimbursement for expenses of up to $10,000.',
-    who: 'Small businesses who have incurred costs from business interruption due to. Emergency Proclamations or costs to implement safety precautions to prevent the spread of COVID-19. Businesses must have less than 1 million in gross annual revenue and 30 or fewer employees. Businesses must also be located in the City and County of Honolulu.',
-    url: 'https://www.oneoahu.org/small-business',
-    status: 'Available Now'
-  },
-  {
-    id: 'hawaii_manufacturing',
-    name: 'Hawaii Manufacturing Assistance Program',
-    what: 'HTDC’s Manufacturing Assistance Program Grant (MAP) offers Hawaii-based manufacturers up to a 20% reimbursement (up to $100,000) on qualified expenses to help Hawaii manufacturers become globally competitive.  Qualifying expenses include: equipment purchases, training, energy efficiency projects, and manufacturing feasibility studies.',
-    who: 'Must conduct manufacturing activities in Hawaii and be categorized as NAICS 31, 32, 33.',
-    url: 'https://www.htdc.org/money/#map',
-    status: 'Available Now'
-  },
-  {
-    id: 'hawaii_malama',
-    name: 'Hawaii Malama Business Loan Program',
-    what: 'This is the most popular loan the Office of Hawaiian Affairs offers that support Native Hawaiian Business owners with loans between $2,500 to $100,000.',
-    who: 'Must be of Native Hawaiian ancestry with a credit score of 600 or higher with an income to debt ratio of no more than 45%.',
-    url: 'https://loans.oha.org/business/malama-business/',
-    status: 'Available Now'
-  },
-  {
-    id: 'hawaii_hua',
-    name: 'Hawaii Hua Kanu Business Loan Program',
-    what: 'The Hua Kanu Business Loan Program is available to Native Hawaiians who own established business. Created on July 17, 2013, the low-cost loans are intended to help these small-businesses expand. It is meant to provide them access to credit and capital that will allow them to grow as well as remain financially viable. Loans between $200,000 to $1,000,000.',
-    who: 'Principals must be Native Hawaiian and verified by current Office of Hawaiian Affairs Registry Card.',
-    url: 'https://loans.oha.org/business/hua-kanu-business-loan/',
-    status: 'Available Now'
-  }
-]
-
 const StatePrograms: React.FC = (props) => {
-  console.log(props)
-  const allEligiblePrograms = props.eligiblePrograms;
-  const elibileStatePrograms = allStatePrograms.filter((stateProgram) => {
-    return allEligiblePrograms.includes(stateProgram.id)
-  })
 
-  const stateProgramList = elibileStatePrograms.map(program => {
+  const stateProgramList = props.eligibleStatePrograms.map(program => {
     return (
       <div className="loan-container">
+        <a name={program.name}></a>
         <label className="top-label">
           {program.status}
         </label>
--- src/components/new_Results.tsx
+++ src/components/new_Results.tsx
@@ -10,27 +10,132 @@ import Footer from "./Footer";
 
 import "./new_results.scss";
 
+const allNationalPrograms = [
+  {
+    id: 'ppp',
+    name: 'Payroll Protection Plan', 
+  },
+  {
+    id: 'eidl',
+    name: 'Economic Injury Disaster Loan Program', 
+  },
+  {
+    id: 'sba',
+    name: 'Small Business Debt Relief Program', 
+  },
+]
+
+const allStatePrograms = [
+  {
+    id: 'ca_small_biz',
+    name: 'California Small Business Finance Center',
+    what: 'Loan guarantees for small businesses',
+    who: 'California small businesses. Loan proceeds can be used for business continuance or to cure "economic injury" as a result of the COVID-19 pandemic.',
+    url: 'https://ibank.ca.gov/small-business-finance-center/',
+    status: 'Available Now'
+  },
+  {
+    id: 'pitt_elf',
+    name: 'Pittsburgh Small Business Emergency Loan Fund',
+    what: 'Loans up to $15,000 with 0% interest offered by the Urban Redevelopment Authority.',
+    who: 'Small businesses located in Pittsburgh with fewer than 30 full-time employees.',
+    url: 'https://www.ura.org/pages/covid-19-small-business-fund',
+    status: 'Available Now'
+  },
+  {
+    id: 'pitt_ura',
+    name: 'Urban Redevelopment Authority of Pittsburgh Recovery Loan',
+    what: 'Loans up to $75,000 with 0% interest for the first year and 2% for the balance of loan term.',
+    who: 'Small businesses located in Pittsburgh with fewer than 30 full-time employees.',
+    url: 'https://www.ura.org/pages/covid-19-small-business-fund',
+    status: 'Available Now'
+  },
+  {
+    id: 'pitt_bridgeway',
+    name: 'Bridgeway Capital Response Fund',
+    what: 'Connecting small businesses in western Pennsylvania to capital when it is needed most.',
+    who: 'Small businesses in western Pennsylvania.',
+    url: 'https://www.bridgewaycapital.org/loans-and-modifications/covid-19-response-fund/',
+    status: 'Available Now'
+  },
+  {
+    id: 'pitt_kiva',
+    name: 'Kiva Pittsburgh Loans',
+    what: 'Kiva helps financially excluded and socially impactful entrepreneurs access capital via crowdfunding.',
+    who: 'Small businesses located in the surrounding areas around Pittsburgh.',
+    url: 'https://www.riversidecenterforinnovation.com/kiva/home',
+    status: 'Available Now'
+  },
+  {
+    id: 'pitt_honeycomb',
+    name: 'Honeycomb Small Business Relief Loan',
+    what: 'Honeycomb Crowdfunded Small Business Relief Loan.',
+    who: 'Small businesses located in the surrounding areas around Pittsburgh.',
+    url: 'https://www.honeycombcredit.com/relief',
+    status: 'Available Now'
+  },
+  {
+    id: 'hawaii_community',
+    name: 'Hawaii Community-Based Economic Development Loan',
+    what: 'CBED offers micro-loans usually up to $50,000 to eligible small businesses that support economic development in their communities.',
+    who: '',
+    url: 'https://invest.hawaii.gov/business/cbed/',
+    status: 'Available Now'
+  },
+  {
+    id: 'hawaii_small_business',
+    name: 'Hawaii Small Business Relief & Recovery Fund',
+    what: 'Small Business Relief & Recovery Fund offers a one-time reimbursement for expenses of up to $10,000.',
+    who: 'Small businesses who have incurred costs from business interruption due to. Emergency Proclamations or costs to implement safety precautions to prevent the spread of COVID-19. Businesses must have less than 1 million in gross annual revenue and 30 or fewer employees. Businesses must also be located in the City and County of Honolulu.',
+    url: 'https://www.oneoahu.org/small-business',
+    status: 'Available Now'
+  },
+  {
+    id: 'hawaii_manufacturing',
+    name: 'Hawaii Manufacturing Assistance Program',
+    what: 'HTDC’s Manufacturing Assistance Program Grant (MAP) offers Hawaii-based manufacturers up to a 20% reimbursement (up to $100,000) on qualified expenses to help Hawaii manufacturers become globally competitive.  Qualifying expenses include: equipment purchases, training, energy efficiency projects, and manufacturing feasibility studies.',
+    who: 'Must conduct manufacturing activities in Hawaii and be categorized as NAICS 31, 32, 33.',
+    url: 'https://www.htdc.org/money/#map',
+    status: 'Available Now'
+  },
+  {
+    id: 'hawaii_malama',
+    name: 'Hawaii Malama Business Loan Program',
+    what: 'This is the most popular loan the Office of Hawaiian Affairs offers that support Native Hawaiian Business owners with loans between $2,500 to $100,000.',
+    who: 'Must be of Native Hawaiian ancestry with a credit score of 600 or higher with an income to debt ratio of no more than 45%.',
+    url: 'https://loans.oha.org/business/malama-business/',
+    status: 'Available Now'
+  },
+  {
+    id: 'hawaii_hua',
+    name: 'Hawaii Hua Kanu Business Loan Program',
+    what: 'The Hua Kanu Business Loan Program is available to Native Hawaiians who own established business. Created on July 17, 2013, the low-cost loans are intended to help these small-businesses expand. It is meant to provide them access to credit and capital that will allow them to grow as well as remain financially viable. Loans between $200,000 to $1,000,000.',
+    who: 'Principals must be Native Hawaiian and verified by current Office of Hawaiian Affairs Registry Card.',
+    url: 'https://loans.oha.org/business/hua-kanu-business-loan/',
+    status: 'Available Now'
+  }
+]
+
 const Results: React.FC = () => {
   const { search } = useLocation();
   const history = useHistory();
-  const [eligiblePrograms, setEligiblePrograms] = useState([]);
 
-  const allPrograms = {
-    'ppp': {name: 'Payroll Protection Plan', url: ''},
-    'eidl': {name: 'Economic Injury Disaster Loan Program', url: ''},
-    'sbdrp': {name: 'Small Business Debt Relief Program', url: ''}
-  }
-
-  const expiredPrograms = [
-    {name: 'State Program', url: ''}
-  ]
+  const [eligibleNationalPrograms, setEligibleNationalPrograms] = useState([]);
+  const [eligibleStatePrograms, setEligibleStatePrograms] = useState([]);
+  const [eligibleProgramIds, setEligibleProgramIds] = useState([]);
 
   useEffect(() => {
     // hacky port of raw js from previous results page, will redo with the new results page
-    const eligibleProgramsParams = new URLSearchParams(search).getAll("eligible");
 
-    setEligiblePrograms(eligibleProgramsParams);
+    const eligibleProgramIds = new URLSearchParams(search).getAll("eligible")
+    
+    setEligibleStatePrograms(allStatePrograms.filter(stateProgram => eligibleProgramIds.includes(stateProgram.id)));
+    setEligibleNationalPrograms(allNationalPrograms.filter(nationalProgram => eligibleProgramIds.includes(nationalProgram.id)));
+    setEligibleProgramIds(eligibleProgramIds);
+    
   }, []);
+  console.log(eligibleNationalPrograms)
+  console.log(eligibleStatePrograms)
 
   return (
     <div>
@@ -46,12 +151,11 @@ const Results: React.FC = () => {
                 If you and your business have an existing relationship with a bank, contact your banker for more information about available relief programs. 
               </p>
               <a name="ppp"></a>
-              {eligiblePrograms.includes('ppp') && <PPPSection/>} 
+              {eligibleProgramIds.includes('ppp') && <PPPSection/>} 
               <a name="eidl"></a>
-              {eligiblePrograms.includes('eidl') && <EIDLProgramSection/>}
-              <a name="expired"></a>
+              {eligibleProgramIds.includes('eidl') && <EIDLProgramSection/>}
               <StatePrograms
-                eligiblePrograms={eligiblePrograms}
+                eligibleStatePrograms={eligibleStatePrograms}
               />
             </div>
             <div className="col-4 right">
@@ -61,18 +165,18 @@ const Results: React.FC = () => {
                     ELIGIBLE
                   </div>
                   <div>
-                    {eligiblePrograms.map(program => 
-                      <div className="sidebar-item"><a href={`#${program}`}>{allPrograms[program]?.name}</a></div>
+                    {eligibleNationalPrograms.map(program => 
+                      <div className="sidebar-item"><a href={`#${program.id}`}>{program.name}</a></div>
                     )}
-                  </div>
-                  <div className="sidebar-title sidebar-expired">
-                    EXPIRED
                   </div>
                   <div>
-                    {expiredPrograms.map(program => 
-                      <div className="sidebar-item"><a href={'#expired'}>{program.name}</a></div>
+                    {eligibleStatePrograms.map(program => 
+                      <div className="sidebar-item"><a href={`#${program.id}`}>{program.name}</a></div>
                     )}
                   </div>
+                  {/* <div className="sidebar-title sidebar-expired">
+                    EXPIRED
+                  </div> */}
                 </div>
               </div>
             </div>
